f7c1cc208ab2ccb53cb62f259abd951d7be3da33
Ivan <iv@Ivans-MacBook-Air.local>
2022-08-07 22:03:35 +0300

    fix fetch

--- pages/portfolio/[slug].jsx
+++ pages/portfolio/[slug].jsx
@@ -1,55 +1,41 @@
 import axios from 'axios';
 import Hero from '../../components/Hero/Hero';
 import ProjectFull from '../../components/ProjectFull/ProjectFull';
-import { MainLayout } from '../../layouts/MainLayout';
+import {MainLayout} from '../../layouts/MainLayout';
 
-export default function Project({ project }) {
-  const tagsArr = project.attributes?.Tags.split(',');
+export default function Project({project}) {
+    const tagsArr = project.attributes?.Tags.split(',');
 
-  return (
-    <MainLayout
-      title={project.attributes?.Seo?.Title}
-      description={project.attributes?.Seo?.Description}
-      keywords={project.attributes?.Seo?.Keywords}>
-      <Hero
-        title={project.attributes?.Title}
-        subtitle={project.attributes?.Subtitle}
-        heroImg={'imgFourth'}
-        tags={tagsArr}
-      />
-      <ProjectFull project={project} />
-    </MainLayout>
-  );
-}
-
-export async function getStaticPaths() {
-  try {
-    const res = await axios.get('https://murmuring-ocean-17174.herokuapp.com/api/works');
-    const projects = await res.data.data;
-    const paths = projects.map((project) => ({ params: { slug: project.attributes.slug } }));
-    
-    return {
-      paths,
-      fallback: true,
-    };
-  } catch (error) {
-    return { error };
-  }
-}
-
-export async function getStaticProps({ params }) {
-  try {
-    const { slug } = params;
-    const res = await axios.get(
-      `https://murmuring-ocean-17174.herokuapp.com/api/works?populate=*&filters\[slug\]=${slug}`,
+    return (
+        <MainLayout
+            title={project.attributes?.Seo?.Title}
+            description={project.attributes?.Seo?.Description}
+            keywords={project.attributes?.Seo?.Keywords}>
+            <Hero
+                title={project.attributes?.Title}
+                subtitle={project.attributes?.Subtitle}
+                heroImg={'imgFourth'}
+                tags={tagsArr}
+            />
+            <ProjectFull project={project}/>
+        </MainLayout>
     );
-    const data = await res.data.data;
-    const project = data[0];
+}
 
-    return {
-      props: { project },
-    };
-  } catch (error) {
-    return { error };
-  }
+
+export async function getServerSideProps({params}) {
+    try {
+        const {slug} = params;
+        const res = await axios.get(
+            `https://murmuring-ocean-17174.herokuapp.com/api/works?populate=*&filters\[slug\]=${slug}`,
+        );
+        const data = await res.data.data;
+        const project = data[0];
+
+        return {
+            props: {project},
+        };
+    } catch (error) {
+        return {error};
+    }
 }
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,58 +1,43 @@
 import urlBuilder from '../../lib/imageUrl';
-import { HeroPost } from '../../components/HeroPost/HeroPost';
+import {HeroPost} from '../../components/HeroPost/HeroPost';
 import PostFull from '../../components/PostFull/PostFull';
-import { MainLayout } from '../../layouts/MainLayout';
+import {MainLayout} from '../../layouts/MainLayout';
 import axios from 'axios';
 
-export default function Post({ post }) {
-  return (
-    <MainLayout
-      title={post.attributes?.Seo?.Title}
-      description={post.attributes?.Seo?.Description}
-      keywords={post.attributes?.Seo?.Keywords}>
-      {post && (
-        <HeroPost
-          title={post.attributes.Title}
-          subtitle={post.attributes.Subtitle}
+export default function Post({post}) {
+    return (
+        <MainLayout
+            title={post.attributes?.Seo?.Title}
+            description={post.attributes?.Seo?.Description}
+            keywords={post.attributes?.Seo?.Keywords}>
+            {post && (
+                <HeroPost
+                    title={post.attributes.Title}
+                    subtitle={post.attributes.Subtitle}
 
-          image={post.attributes.postImage.data ? urlBuilder(post.attributes.postImage.data.attributes.url) : ''}
-        />
-      )}
-      {post && <PostFull content={post.attributes.body} />}
-    </MainLayout>
-  );
-}
-
-export async function getStaticPaths() {
-  try {
-    const res = await axios.get('https://murmuring-ocean-17174.herokuapp.com/api/posts');
-    const posts = await res.data.data;
-    const paths = posts.map((post) => ({ params: { slug: post.attributes.Slug } }));
-  
-    return {
-      paths,
-      fallback: false,
-    };
-  } catch (error) {
-    return { error };
-  }
-}
-
-export async function getStaticProps({ params }) {
-  try {
-    const { slug } = params;
-    const res = await axios.get(
-      `https://murmuring-ocean-17174.herokuapp.com/api/posts?populate=*&filters\[Slug\]=${slug}`,
+                    image={post.attributes.postImage.data ? urlBuilder(post.attributes.postImage.data.attributes.url) : ''}
+                />
+            )}
+            {post && <PostFull content={post.attributes.body}/>}
+        </MainLayout>
     );
-    const data = await res.data.data;
-    const post = data[0];
+}
 
-    console.log(data);
 
-    return {
-      props: { post },
-    };
-  } catch (error) {
-    return { error };
-  }
+export async function getServerSideProps({params}) {
+    try {
+        const {slug} = params;
+        const res = await axios.get(
+            `https://murmuring-ocean-17174.herokuapp.com/api/posts?populate=*&filters\[Slug\]=${slug}`,
+        );
+        const data = await res.data.data;
+        const post = data[0];
+        
+
+        return {
+            props: {post},
+        };
+    } catch (error) {
+        return {error};
+    }
 }
