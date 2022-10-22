50a2fc9748b1c4245e62742b600c7e416603df3b
Ivan <iv@Ivans-MacBook-Air.local>
2022-10-22 12:24:52 +0300

    fix 404

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,24 +1,17 @@
-import {MainLayout} from "../layouts/MainLayout";
+import { MainLayout } from '../layouts/MainLayout';
 
-export default function FourOhFour(){
-    return (
-        <MainLayout
-        title={'404 Page Not Found - LTV:CAC agency'}
-        description={
-            '404 Page Not Found'
-        }
-    >
-        <div className="pt-60 pb-80 pt-xl-190 pb-xl-190">
-            <div className="container ph-20">
-                <div className="row">
-                    <div className="col-5 col-xl-5 m-auto">
-                        <h1 className="mb-4">404 - Page Not Found</h1>
-                    </div>
-                </div>
+export default function FourOhFour() {
+  return (
+    <MainLayout title={'404 Page Not Found - LTV:CAC agency'} description={'404 Page Not Found'}>
+      <div className="pt-60 pb-80 pt-xl-190 pb-xl-190">
+        <div className="container ph-20">
+          <div className="row">
+            <div className="col-5 col-xl-5 m-auto">
+              <h1 className="mb-4">404 - Page Not Found</h1>
             </div>
+          </div>
         </div>
+      </div>
     </MainLayout>
-    );
-
+  );
 }
-
