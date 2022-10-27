34ad0870e4884e592ad71011e2380c3e2dd5aedf
Naimanshalov Ilias <mail@nabster.name>
2022-10-27 16:41:19 +0600

    404 error page design without cookie

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,19 +1,49 @@
 import {MainLayout} from "../layouts/MainLayout";
+import style from "../styles/404.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function FourOhFour(){
     return (
         <MainLayout
         title={'404 Page Not Found - LTV:CAC agency'}
         description={
-            '404 Page Not Found'
+            'We can\'t seem to find the page you\'re looking for.\n' +
+            'Error code: 404\n'
         }
     >
         <div className="pt-140 pb-120 pb-md-100 pt-md-100 pt-xl-190 pb-xl-190">
-            <div className="container ph-20">
+            <div className={`container ph-20 ${style.errorPage}`}>
                 <div className="row">
-                    <div className="col-9 col-md-7 col-xl-6 m-auto">
-                        <h1 className="mb-4">404 - Page Not Found</h1>
-                        <p>Whoops! Looks like the page you are looking for doesn&apos;t exist.</p>
+                    <div className="col-6 col-md-6 col-xl-6 m-auto">
+                        <h1 className="mb-4">Oops!</h1>
+                        <p className={style.message}>We can&apos;t seem to find the page you&apos;re looking for.<br />
+                            Error code: 404</p>
+
+                        <div className={style.helpBlock}>
+                            <span className={style.helpText}>Here are some helpful links instead:</span>
+                            <div className={style.linksBlock}>
+                                <Link href={'/'}>
+                                    <a className={style.link}>Home</a>
+                                </Link>
+                                <Link href={'/portfolio'}>
+                                    <a className={style.link}>Portfolio</a>
+                                </Link>
+                                <Link href={'/services'}>
+                                    <a className={style.link}>Services</a>
+                                </Link>
+                                <Link href={'/contacts'}>
+                                    <a className={style.link}>Contacts</a>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <Image src="/img/404.png"
+                            width={575}
+                            height={244}
+                        />
+                        <span className={style.errorCode}>Error code: 404</span>
                     </div>
                 </div>
             </div>
--- pages/sitemap/test.jsx
+++ pages/sitemap/test.jsx
@@ -1,43 +1,6 @@
 import axios from "axios";
+import {getServerSideSitemap} from "next-sitemap";
 
-export default function test ({posts}) {
-    // const url = 'https://www.ltvcac.agency/porfolio/';
-    // let slug = [
-    //     'portfolio-interweave',
-    //     'portfolio-loft_denim',
-    //     'portfolio-togezzer',
-    //     'portfolio-keyou',
-    //     'portfolio-matahari_hills'
-    // ];
-    // let urls = []
-
-    const url = 'https://www.ltvcac.agency/posts/'
-
-    console.log(posts);
-
-    slug.map(function (el){
-       urls.push(url+el);
-    });
-
-    return (
-    `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
-    ${posts && posts.map((obj) =>{
-        <url>
-            <loc>{url + obj.attributes.slug}</loc>
-            <lastmod>{obj.attributes.updatedAt}</lastmod>
-            <changefreq>daily</changefreq>
-            <priority>0.7</priority>
-        </url>
-    }) }
-    
-    
-    </urlset>`
-    );
-
-    console.log(urls);
-
-}
 export async function getServerSideProps() {
     try {
         const post = await axios.get(`${process.env.API_URL}/blogs?sort=createdAt:desc&populate=*`);
@@ -46,12 +9,22 @@ export async function getServerSideProps() {
         // const porfolio = await axios.get(`${process.env.API_URL}/portfolios?populate=*`);
         // const porfolios = await porfolio.data.data;
 
-        return {
-            props: {
-                posts
+        const url = 'https://www.ltvcac.agency/posts/';
+        //console.log(posts)
+        const fields = [
+            {
+                loc: url,
+                lastmod: new Date().toString(),
             },
-        };
+            {
+                loc: url,
+                lastmod: new Date().toString(),
+            }
+        ]
+        return getServerSideSitemap(ctx, fields, posts)
     } catch (error) {
         return {error};
     }
 }
+
+export default function Sitemap(posts){};
