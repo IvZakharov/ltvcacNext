64b31035002e101e67ce12269d3094665ac14738
Naimanshalov Ilias <mail@nabster.name>
2022-11-04 20:39:42 +0600

    return schema org

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -14,7 +14,6 @@ export const MainLayout = ({ children, title, description, keywords, color, date
   const [offset, setOffset] = useState(0);
   const [Cookie, setAlert] = useState(true);
   const router = useRouter()
-  console.log(router.pathname)
 
   useEffect(() => {
     const onScroll = () => setOffset(window.scrollY);
@@ -38,7 +37,7 @@ export const MainLayout = ({ children, title, description, keywords, color, date
         <meta name="description" content={description} />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <meta name="facebook-domain-verification" content="1yko0pr5s3tuwq9dl4iokow2q6yb1j" />
-
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: `{"@context":"https://schema.org","@graph":[{"@type":"Organization","@id":"https://www.ltvcac.agency/#organization","name":"LTVCAC Agency","url":"https://www.ltvcac.agency/","sameAs":["https://linkedin.com/company/ltvcac-agency","https://facebook.com/ltvcac","https://instagram.com/ltvcac.agency",],"logo":{"@type":"ImageObject","@id":"https://seoquick.com.ua/#logo","url":"https://seoquick.com.ua/wp-content/uploads/2021/10/seoquick-600x600-1.png","width":336,"height":333,"caption":"LTVCAC"},"image":{"@id":"https://www.ltvcac.agency/#logo"}},{"@type":"WebSite","@id":"https://www.ltvcac.agency/#website","url":"https://www.ltvcac.agency/","name":"LTVCAC Agency","publisher":{"@id":"https://www.ltvcac.agency/#organization"}},{"@type":"WebPage","@id":"https://www.ltvcac.agency${router.pathname}#webpage","url":"https://www.ltvcac.agency${router.pathname}","inLanguage":"en-EN","name":"${title}","isPartOf":{"@id":"https://www.ltvcac.agency/#website"},"about":{"@id":"https://www.ltvcac.agency/#organization"},"datePublished":"2015-06-17T13:45:57+03:00","description":"${description}"}]}`}} />
       </Head>
 
       <Header onClickMenu={(bool) => setOpenedMenu(bool)} />
