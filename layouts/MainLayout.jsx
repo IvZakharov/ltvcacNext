ff3cc496348e66b78d2c753981ca138972391279
Naimanshalov Ilias <mail@nabster.name>
2022-11-04 15:13:45 +0600

    schema.org organization and website

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -7,11 +7,14 @@ import { ToTopBtn } from '../components/ToTopBtn/ToTopBtn';
 import { useEffect, useState } from 'react';
 import TagManager from 'react-gtm-module';
 import CookieAlert from "../components/CookieAlert/CookieAlert";
+import {useRouter} from "next/router";
 
-export const MainLayout = ({ children, title, description, keywords, color }) => {
+export const MainLayout = ({ children, title, description, keywords, color, date }) => {
   const [openedMenu, setOpenedMenu] = useState(false);
   const [offset, setOffset] = useState(0);
   const [Cookie, setAlert] = useState(true);
+  const router = useRouter()
+  console.log(router.pathname)
 
   useEffect(() => {
     const onScroll = () => setOffset(window.scrollY);
@@ -35,6 +38,59 @@ export const MainLayout = ({ children, title, description, keywords, color }) =>
         <meta name="description" content={description} />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <meta name="facebook-domain-verification" content="1yko0pr5s3tuwq9dl4iokow2q6yb1j" />
+        <script type="application/ld+json">
+          {`{
+                "@context":"https://schema.org",
+                "@graph":[
+                  {
+                      "@type":"Organization",
+                     "@id":"https://www.ltvcac.agency/#organization",
+                     "name":"LTVCAC Agency",
+                     "url":"https://www.ltvcac.agency/",
+                     "sameAs":[
+                          "https://linkedin.com/company/ltvcac-agency",
+                          "https://facebook.com/ltvcac",
+                          "https://instagram.com/ltvcac.agency",
+                      ],
+                         "logo":{
+                         "@type":"ImageObject",
+                         "@id":"https://seoquick.com.ua/#logo",
+                         "url":"https://seoquick.com.ua/wp-content/uploads/2021/10/seoquick-600x600-1.png",
+                         "width":336,
+                         "height":333,
+                         "caption":"LTVCAC"
+                         },
+                         "image":{
+                              "@id":"https://www.ltvcac.agency/#logo"
+                              }
+                     },
+                     {
+                          "@type":"WebSite",
+                          "@id":"https://www.ltvcac.agency/#website",
+                          "url":"https://www.ltvcac.agency/",
+                          "name":"LTVCAC Agency",
+                          "publisher":{
+                              "@id":"https://www.ltvcac.agency/#organization"
+                              }
+                     },
+                     {
+                          "@type":"WebPage",
+                          "@id":"https://www.ltvcac.agency${router.pathname}#webpage",
+                          "url":"https://www.ltvcac.agency${router.pathname}",
+                          "inLanguage":"en-EN",
+                          "name":"${title}",
+                          "isPartOf":{
+                              "@id":"https://www.ltvcac.agency/#website"
+                              },
+                          "about":{
+                              "@id":"https://www.ltvcac.agency/#organization"
+                              },
+                          "datePublished":"2015-06-17T13:45:57+03:00",
+                          "description":"${description}"
+                              }
+                        ]
+                    }`}
+        </script>
       </Head>
 
       <Header onClickMenu={(bool) => setOpenedMenu(bool)} />
