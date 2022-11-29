f801811259c6da3d4590285569536888fd166e7c
Naimanshalov Ilias <mail@nabster.name>
2022-11-29 12:15:09 +0600

    redacted Cookie Alert

--- components/CookieAlert/CookieAlert.jsx
+++ components/CookieAlert/CookieAlert.jsx
@@ -13,13 +13,13 @@ function CookieAlert({ onClickClose }){
                 <div className={`row justify-content-center ${style.mob}`}>
                     <div className={style.textBlock}>
                         <p>
-                            We use cookies to improve your experience on the website.
-                            By staying on our site you consent to our use of cookies.
+                            We use cookies to improve your experience on this website.
+                            By staying on the site you consent to our use of cookies.
                         </p>
                     </div>
                     <div className={style.buttonBlock}>
                         <button onClick={() => setOff()}>
-                            Good
+                            Got it
                         </button>
                     </div>
                 </div>
