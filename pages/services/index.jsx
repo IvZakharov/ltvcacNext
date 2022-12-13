c0f071cd1644d6f306fb3c526f68956022de5243
Naimanshalov Ilias <mail@nabster.name>
2022-12-13 19:14:24 +0600

    styled service page

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -23,7 +23,7 @@ export default function services() {
           <div className="container">
             <div className="row">
               <div className="col-12 col-md-6 my-auto">
-                <div className="marginHomeLeft ta-center">
+                <div className="marginHomeLeft" style={{textAlign:"right", textTransform:"uppercase"}}>
                   <h2 className="h1title">
                     Paid Marketing
                     <br/>
@@ -33,14 +33,14 @@ export default function services() {
               </div>
               <div className="col-12 col-md-6 col-xl-6">
                 <div className="ph-20 ml-120 mt-22 mb-22">
-                  <h3 className="h3 mb-20 ta-center">
+                  <h3 className="h3 mb-20">
                     Data-driven digital marketing for your customer’s base growing
                   </h3>
-                  <p className="text-large mb-20">
+                  <p className="textFonts mb-20">
                     Despite the COVID‑19 pandemic, all digital ad channels demonstrate strong growth
                     in the world market this year.
                   </p>
-                  <ul className="text-large mb-20">
+                  <ul className="textFonts mb-20">
                     <li>Google Ads +29%</li>
                     <li>Apple Search Ads +87%</li>
                     <li>Instagram-Facebook +23%</li>
@@ -64,7 +64,7 @@ export default function services() {
             <div className="row">
               <div className="col-12 col-md-6 order-md-1 my-auto">
                 <div className="marginHomeRight ta-center">
-                  <h2 className="h1title">
+                  <h2 className="h1title" style={{textAlign:'left'}}>
                     MOBILE &
                     <br/>
                     WEBSITE
@@ -73,14 +73,14 @@ export default function services() {
                   </h2>
                 </div>
               </div>
-              <div className="col-12 col-md-6 col-xl-4 mt-22 mb-22 mr-120">
-                <div className="ph-20">
-                  <h3 className="h3 mb-20 ta-center">Why does your company need tracking?</h3>
-                  <p className="text-large mb-20">
+              <div className="col-12 col-md-6 ">
+                <div className="ph-20 mt-22 mb-22 mr-120" style={{width:"60%"}}>
+                  <h3 className="h3 mb-20" style={{textAlign: "right"}}>Why does your company need tracking?</h3>
+                  <p className="textFonts mb-20">
                     Tracking report is your dashboard that shows where and how your business is
                     flying.
                   </p>
-                  <p className="text-large mb-20">
+                  <p className="textFonts mb-20">
                   <span className="d-block">
                     Imagine you just got a seat on a jet plane. I&apos;s time to take off and the
                     pilot says:
@@ -93,7 +93,7 @@ export default function services() {
                     - Yeah, it’s okay. We might miss the right city but I’ll get us…somewhere.
                   </span>
                   </p>
-                  <p className="text-large mb-20">
+                  <p className="textFonts mb-20">
                     The jet plane is your business. It can get you to the destination or it can crash.
                   </p>
                   
@@ -110,7 +110,7 @@ export default function services() {
           <div className="container">
             <div className="row">
               <div className="col-12 col-md-6 my-auto">
-                <div className="marginHomeLeft ta-center">
+                <div className="marginHomeLeft" style={{textAlign:'right'}}>
                   <h2 className="h1title">
                     NO-CODE
                     <br/>
@@ -118,26 +118,26 @@ export default function services() {
                   </h2>
                 </div>
               </div>
-              <div className="col-12 col-md-6 col-xl-4 mb-22 mt-22 ml-120">
-                <div className="ph-20">
-                  <h3 className="h3 mb-20 ta-center">Why does your company need automation?</h3>
-                  <p className="text-large mb-20">
+              <div className="col-12 col-md-6 ">
+                <div className="ph-20 mb-22 mt-22 ml-120" style={{width:"60%"}}>
+                  <h3 className="h3 mb-20 ta-left">Why does your company need automation?</h3>
+                  <p className="textFonts mb-20">
                     Airplane autopilot quite a familiar thing these days but it didn&apos;t exist
                     once.
                   </p>
-                  <p className="text-large mb-20">
+                  <p className="textFonts mb-20">
                     Nonetheless people successfully crossed oceans and flew around the globe without
                     it.
                   </p>
-                  <p className="text-large mb-20">
+                  <p className="textFonts mb-20">
                     Over time, the autopilot system was invented, which greatly facilitated the life
                     of line pilots and reduced airline fuel costs.
                   </p>
-                  <p className="text-large mb-20">
+                  <p className="textFonts mb-20">
                     The autopilot is much more fuel efficient and relieves the pilot of a huge burden,
                     thereby increasing safety and range.
                   </p>
-                  <p className="text-large mb-20">Automation is your autopilot system.</p>
+                  <p className="textFonts mb-20">Automation is your autopilot system.</p>
                   <Link href="/services/no-code-automation">
                     <a className="button button--secondary d-block">FIND OUT MORE</a>
                   </Link>
@@ -159,12 +159,12 @@ export default function services() {
                   </h2>
                 </div>
               </div>
-              <div className="col-12 col-md-6 col-xl-4 mt-22 mb-22 mr-120">
-                <div className="ph-20">
-                  <h3 className="h3 mb-20 ta-center">
+              <div className="col-12 col-md-6 ">
+                <div className="ph-20 mt-22 mb-22 mr-120"  style={{width:"60%"}}>
+                  <h3 className="h3 mb-20" style={{textAlign:'right'}}>
                     Why does your company need customer research?
                   </h3>
-                  <p className="text-large mb-20">
+                  <p className="textFonts mb-20">
                     Misidentifying your customers’ pains or their priorities to perform JBTD results
                     in your team working for weeks/months on problems that your customers aren’t
                     willing to pay for, which is $xxx.xx spent in vain.
@@ -181,7 +181,7 @@ export default function services() {
           <div className="container">
             <div className="row">
               <div className="col-12 col-md-6 my-auto">
-                <div className="marginHomeLeft ta-center">
+                <div className="marginHomeLeft" style={{textAlign:'right'}}>
                   <h2 className="h1title">
                     App Store
                     <br/>
@@ -189,12 +189,12 @@ export default function services() {
                   </h2>
                 </div>
               </div>
-              <div className="col-12 col-md-6 col-xl-4 mt-22 mb-22 ml-120">
-                <div className="ph-20">
-                  <h3 className="h3 mb-20 ta-center">
+              <div className="col-12 col-md-6">
+                <div className="ph-20 mt-22 mb-22 ml-120" style={{width:"60%"}}>
+                  <h3 className="h3 mb-20 ta-left">
                     Site and app Tracking system setup services
                   </h3>
-                  <p className="text-large mb-20">
+                  <p className="textFonts mb-20">
                     We can build system compile marketing data and sales get exact selling costs to sales ratio, while monitoring ad channels and events inside
                     sales funnel.
                   </p>
